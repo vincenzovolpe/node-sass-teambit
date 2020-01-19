fffa0ec6190be7aabc8768d5ee90b6f84252180e
vincenzovolpe <vinci792010@gmail.com>
2020-01-19 16:49:41 +0100

    Creato effetto comprsa scomparsa navbar header

--- public/js/app.js
+++ public/js/app.js
@@ -17899,6 +17899,28 @@ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js"
 
 __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
 
+$(document).ready(function () {
+  // Comportamento dell'header che cambia in base allo scroll
+  $('.nav_header').removeClass('border');
+  $('.nav_header').children('.container').removeClass('nobefore');
+  $('.nav_header').removeClass('invisibile');
+  $(window).scroll(function () {
+    $('.navbar').addClass('invisibile');
+
+    if ($(document).scrollTop() > 400) {
+      $('.nav_header').children('.container').addClass('nobefore');
+      $('.nav_header').addClass('border');
+      $('.nav_header').removeClass('invisibile');
+    }
+
+    if ($(document).scrollTop() == 0) {
+      $('.nav_header').removeClass('border');
+      $('.nav_header').children('.container').removeClass('nobefore');
+      $('.nav_header').removeClass('invisibile');
+    }
+  });
+});
+
 /***/ }),
 
 /***/ "./src/scss/app.scss":
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,22 @@
 var $ = require('jquery');
 require('bootstrap');
+
+$(document).ready(function() {
+    // Comportamento dell'header che cambia in base allo scroll
+    $('.nav_header').removeClass('border');
+    $('.nav_header').children('.container').removeClass('nobefore');
+    $('.nav_header').removeClass('invisibile');
+    $(window).scroll(function() {
+        $('.navbar').addClass('invisibile');
+        if ($(document).scrollTop() > 400) {
+            $('.nav_header').children('.container').addClass('nobefore');
+            $('.nav_header').addClass('border');
+            $('.nav_header').removeClass('invisibile');
+        }
+        if ($(document).scrollTop() == 0) {
+            $('.nav_header').removeClass('border');
+            $('.nav_header').children('.container').removeClass('nobefore');
+            $('.nav_header').removeClass('invisibile');
+        }
+    });
+});
